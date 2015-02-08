b43ece9cda0705a8b991e44fb988b63b20725b85
dgliosca <diego.gliosca@gmail.com>
2015-02-08 11:35:18 +0000

    substitued for with foreach

--- JavaProblem/src/main/java/SuperMarketPlusPlus.java
+++ JavaProblem/src/main/java/SuperMarketPlusPlus.java
@@ -4,15 +4,12 @@ import java.util.List;
 
 public class SuperMarketPlusPlus {
 
-	private static List<Item> items = null;
+    private static List<Item> items = null;
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		
+    public static void main(String[] args) {
+
         System.out.println("Starting Supermarket Plus Plus");
-		
+
         items = new ArrayList<Item>();
         items.add(new Item("Thermal Vest", 10, 20));
         items.add(new Item("Aged Brie", 2, 0));
@@ -22,80 +19,55 @@ public class SuperMarketPlusPlus {
         items.add(new Item("Ginger Cake", 3, 6));
 
         updateQuality();
-}
+    }
 
 
-	
-    public static void updateQuality()
-    {
-        for (int i = 0; i < items.size(); i++)
-        {
-            if ((!"Aged Brie".equals(items.get(i).getName())) && !"Backstage Passes".equals(items.get(i).getName())) 
-            {
-                if (items.get(i).getQuality() > 0)
-                {
-                    if (!"Sulfuras".equals(items.get(i).getName()))
-                    {
-                        items.get(i).setQuality(items.get(i).getQuality() - 1);
+    public static void updateQuality() {
+        for (Item item : items) {
+            if ((!"Aged Brie".equals(item.getName())) && !"Backstage Passes".equals(item.getName())) {
+                if (item.getQuality() > 0) {
+                    if (!"Sulfuras".equals(item.getName())) {
+                        item.setQuality(item.getQuality() - 1);
                     }
                 }
-            }
-            else
-            {
-                if (items.get(i).getQuality() < 50)
-                {
-                    items.get(i).setQuality(items.get(i).getQuality() + 1);
+            } else {
+                if (item.getQuality() < 50) {
+                    item.setQuality(item.getQuality() + 1);
 
-                    if ("Backstage Passes".equals(items.get(i).getName()))
-                    {
-                        if (items.get(i).getSellIn() < 11)
-                        {
-                            if (items.get(i).getQuality() < 50)
-                            {
-                                items.get(i).setQuality(items.get(i).getQuality() + 1);
+                    if ("Backstage Passes".equals(item.getName())) {
+                        if (item.getSellIn() < 11) {
+                            if (item.getQuality() < 50) {
+                                item.setQuality(item.getQuality() + 1);
                             }
                         }
 
-                        if (items.get(i).getSellIn() < 6)
-                        {
-                            if (items.get(i).getQuality() < 50)
-                            {
-                                items.get(i).setQuality(items.get(i).getQuality() + 1);
+                        if (item.getSellIn() < 6) {
+                            if (item.getQuality() < 50) {
+                                item.setQuality(item.getQuality() + 1);
                             }
                         }
                     }
                 }
             }
 
-            if (!"Sulfuras".equals(items.get(i).getName()))
-            {
-                items.get(i).setSellIn(items.get(i).getSellIn() - 1);
+            if (!"Sulfuras".equals(item.getName())) {
+                item.setSellIn(item.getSellIn() - 1);
             }
 
-            if (items.get(i).getSellIn() < 0)
-            {
-                if (!"Aged Brie".equals(items.get(i).getName()))
-                {
-                    if (!"Backstage Passes".equals(items.get(i).getName()))
-                    {
-                        if (items.get(i).getQuality() > 0)
-                        {
-                            if (!"Sulfuras".equals(items.get(i).getName()))
-                            {
-                                items.get(i).setQuality(items.get(i).getQuality() - 1);
+            if (item.getSellIn() < 0) {
+                if (!"Aged Brie".equals(item.getName())) {
+                    if (!"Backstage Passes".equals(item.getName())) {
+                        if (item.getQuality() > 0) {
+                            if (!"Sulfuras".equals(item.getName())) {
+                                item.setQuality(item.getQuality() - 1);
                             }
                         }
+                    } else {
+                        item.setQuality(item.getQuality() - item.getQuality());
                     }
-                    else
-                    {
-                        items.get(i).setQuality(items.get(i).getQuality() - items.get(i).getQuality());
-                    }
-                }
-                else
-                {
-                    if (items.get(i).getQuality() < 50)
-                    {
-                        items.get(i).setQuality(items.get(i).getQuality() + 1);
+                } else {
+                    if (item.getQuality() < 50) {
+                        item.setQuality(item.getQuality() + 1);
                     }
                 }
             }
