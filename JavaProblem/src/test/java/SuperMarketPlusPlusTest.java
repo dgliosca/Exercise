0f0b6e1208a5c46f1c3e5f04a8b9ad8b6abffcb0
dgliosca <diego.gliosca@gmail.com>
2015-02-08 11:42:39 +0000

    writing tests for the updateQuality method in order to avoid to break the system.

--- JavaProblem/src/test/java/SuperMarketPlusPlusTest.java
+++ JavaProblem/src/test/java/SuperMarketPlusPlusTest.java
@@ -1,12 +1,24 @@
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
 
 
 public class SuperMarketPlusPlusTest {
 
 	@Test
 	public void testTheTruth() {
-		assertTrue(true);
+		List<Item> items = new ArrayList<Item>();
+		items.add(new Item("Thermal Vest", 10, 20));
+		SuperMarketPlusPlus superMarketPlusPlus = new SuperMarketPlusPlus();
+		superMarketPlusPlus.updateQuality(items);
+
+		assertThat(items.get(0).getName(), equalTo("Thermal Vest"));
+		assertThat(items.get(0).getQuality(), equalTo(19));
+		assertThat(items.get(0).getSellIn(), equalTo(9));
+
 	}
 }
